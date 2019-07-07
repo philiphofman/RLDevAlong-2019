2e211b2b32a48a887a2c115051d7d39394903890
Philip Hofman <philip.hofman@gmail.com>
2019-07-06 21:41:48 -0700

    Delete game_states.py

--- Roguelike DevAlong/game_states.py
+++
@@ -1,7 +0,0 @@
-# An 'Enum' is a set of named values that won't change, so it's perfect for things like game states. The numbers don't really mean anything so the auto() helper just numbers them automatically.
-
-from enum import Enum, auto
-
-class GameStates(Enum):
-	PLAYERS_TURN = auto()
-	ENEMY_TURN = auto()
