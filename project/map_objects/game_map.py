affabd4a01cbd5b3e0173519e864e43e8434dc27
Philip Hofman <philip.hofman@gmail.com>
2019-08-31 16:21:57 -0700

    Finished first half of Part 9

--- project/map_objects/game_map.py
+++ project/map_objects/game_map.py
@@ -2,8 +2,9 @@ from random import randint
 import tcod as libtcod
 
 from entity import Entity
+from game_messages import Message
 from render_functions import RenderOrder
-from item_functions import heal
+from item_functions import heal, cast_lightning, cast_fireball
 
 from components.ai import BasicMonster
 from components.fighter import Fighter
@@ -191,8 +192,17 @@ class GameMap:
 			y = randint(room.y1 + 1, room.y2 - 1)
 			
 			if not any([entity for entity in entities if entity.x == x and entity.y == y]):
-				item_component = Item(use_function=heal, amount=4)
-				item = Entity(x, y, '!', libtcod.violet, 'Healing Potion', render_order=RenderOrder.ITEM, item=item_component)
+				item_chance = randint(0,100)
+				
+				if item_chance < 70:
+					item_component = Item(use_function=heal, amount=4)
+					item = Entity(x, y, '!', libtcod.violet, 'Healing Potion', render_order=RenderOrder.ITEM, item=item_component)
+				elif item_chance < 85:
+					item_component = Item(use_function=cast_fireball, targeting=True, targeting_message=Message('Left-click a target tile for the fireball, or right-click to cancel.', libtcod.light_cyan), damage=12, radius=3)
+					item = Entity(x, y, '#', libtcod.red, 'Fireball Scroll', render_order=RenderOrder.ITEM, item=item_component)
+				else:
+					item_component = Item(use_function=cast_lightning, damage=20, maximum_range = 5)
+					item = Entity(x, y, '#', libtcod.yellow, 'Lightning Scroll', render_order=RenderOrder.ITEM, item=item_component)
 				
 				entities.append(item)
 	
